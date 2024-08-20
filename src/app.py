a4361141b431d5c355285d70970c096d7ba1f364
Dheeraj Poolavaram <poolavaram.dheeraj@gmail.com>
2024-08-21 00:27:06 +0200

    Refactor app.py

--- src/app.py
+++ src/app.py
@@ -5,25 +5,28 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
-from src.ui_components import (
-    configure_sidebar,
-    configure_chat_interface,
-    configure_db_connection,
-    display_help,
-    display_loading_indicator,
-    export_query_results,
-    handle_errors,
-)
+from src.utils.ui_components import configure_sidebar, configure_chat_interface, configure_db_connection, display_help
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_types import AgentType
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain_groq import ChatGroq
-from src.db_utils import configure_db
-from src.langchain_utils import custom_prompt
-import groq
+from src.utils.langchain_utils import custom_prompt
+
+# Set the background image
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://archive.webdesignhot.com/wp-content/uploads/2013/04/Colorful-Abstract-Waves-on-Black-Background-Vector-Graphic_thumb.jpg");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}
+</style>"""
+st.markdown(background_image, unsafe_allow_html=True)
 
 # Streamlit app configuration
-st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+#st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
 st.title("LangChain: Chat with SQL DB")
 
 # Sidebar Configuration
@@ -49,8 +52,8 @@ else:
         prompt_template=custom_prompt,
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         handle_parsing_errors=True,
-        max_iterations=10,  # Increase the iteration limit
-        max_execution_time=60  # Increase the time limit (in seconds)
+        max_iterations=25,  # Increase the iteration limit
+        max_execution_time=120  # Increase the time limit (in seconds)
     )
 
     # Chat Interface
